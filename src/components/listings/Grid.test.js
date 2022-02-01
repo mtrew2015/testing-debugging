7c52cb3c41bcec2a1fdf57cc01347d4e2eb0345d
Michael Trew <mtrew@wsgc.com>
2022-02-01 13:12:35 -0500

    adding tests for string matchers

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -9,3 +9,13 @@ test("number of items should equal 12", () => {
 test("number of items should be greater than or equal to 12", () => {
   expect(numItems).toBeGreaterThanOrEqual(12);
 });
+
+const dataTest = data[0].title;
+
+test("There is a JS in the title", () => {
+  expect(dataTest).toMatch(/JS/);
+});
+
+test("The title contains React ", () => {
+  expect(dataTest).toContain('ReactJS');
+});
